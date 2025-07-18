66d06eba3e9f97af7dbc53381cfe63e6639d81d7
Jordi Giménez Vélez <vinjihub>
2025-07-18 08:46:49 +0200

    buscado funcione base db

--- storage/storage.go
+++ storage/storage.go
@@ -1,13 +1,36 @@
-var Countries []models.Country
+package storage
 
-func LoadCountriesFromFile() {
-	data, err := os.ReadFile("data/countries.json")
-	if err == nil {
-		json.Unmarshal(data, &Countries)
+import (
+	"log"
+	"os"
+
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+
+	"backend_golang/models"
+)
+
+var DB *gorm.DB
+
+func Connect() {
+	// Asegurarse de que el directorio de la base de datos exista
+	err := os.MkdirAll("database", os.ModePerm)
+	if err != nil {
+		log.Fatal("No se pudo crear el directorio de base de datos:", err)
 	}
-}
 
-func SaveCountriesToFile() {
-	data, _ := json.MarshalIndent(Countries, "", "  ")
-	_ = os.WriteFile("data/countries.json", data, 0644)
+	// Conectarse a SQLite y crear/abrir el archivo
+	db, err := gorm.Open(sqlite.Open("database/data.db"), &gorm.Config{})
+	if err != nil {
+		log.Fatal("Error al conectar con la base de datos:", err)
+	}
+
+	// Migrar el modelo Country (crear tabla si no existe)
+	err = db.AutoMigrate(&models.Country{})
+	if err != nil {
+		log.Fatal("Error en AutoMigrate:", err)
+	}
+
+	log.Println("Conexión a la base de datos establecida y modelo migrado.")
+	DB = db
 }
